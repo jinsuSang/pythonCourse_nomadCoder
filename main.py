bf45db4333023973a5edbbb9f1cd8a770284fd9e
jinsuSang <twtz0309@naver.com>
2020-02-09 22:17:01 +0900

    Conditionals part One

--- main.py
+++ main.py
@@ -1,2 +1,8 @@
-days = ('Mon', 'Tue', 'Wed', 'Thur', 'Fri')
-print(days[2])
+def plus(num1, num2):
+    if (type(num1) is int or type(num1) is float) and (type(num2) is int or type(num2) is float):
+        return num1 + num2
+    else:
+        return None
+
+
+print(plus(1, 5.2))
