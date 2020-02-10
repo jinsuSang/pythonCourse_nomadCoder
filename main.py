e8d3c2dd9ba497560f42dfae4496b8567d676be0
jinsuSang <twtz0309@naver.com>
2020-02-10 16:06:37 +0900

    Extracting Indeed Pages part Two

--- main.py
+++ main.py
@@ -1,8 +1,16 @@
-def plus(num1, num2):
-    if (type(num1) is int or type(num1) is float) and (type(num2) is int or type(num2) is float):
-        return num1 + num2
-    else:
-        return None
+import pip._vendor.requests as requests
+from bs4 import BeautifulSoup
+indeed_url = requests.get(
+    "https://kr.indeed.com/%EC%B7%A8%EC%97%85?as_and=python&as_phr=&as_any=&as_not=&as_ttl=&as_cmp=&jt=all&st=&as_src=&radius=25&l=%EC%9D%B8%EC%B2%9C&fromage=any&limit=50&sort=&psf=advsrch&from=advancedsearch")
 
 
-print(plus(1, 5.2))
+indeed_soup = BeautifulSoup(indeed_url.text, "html.parser")
+
+pagination = indeed_soup.find("div", {"class": "pagination"})
+
+links = pagination.find_all('a')
+pages = []
+
+for link in links[:-1]:
+    pages.append(int(link.string))
+max_page = pages[-1]
